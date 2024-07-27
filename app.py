ef9dd7be8c60aa604c0c40b9d27e3e462f1bef90
TommyClemz1 <99149627+TommyClemz1@users.noreply.github.com>
2024-07-27 06:07:00 +0100

    Add spaCy model files using Git LFS

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:341c6e7acb5df8a8d98cab6a8a2782d8c72f29d5ed7df56a083094d439517de7
-size 5458
+oid sha256:b2bcae9d2d6614649978dbb3efa758ce2b918673d862b0ce0053749a5893cc42
+size 1433
